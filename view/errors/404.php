4c1c72933be6b8ad1ae4d54f1047debfd5461949
Aminulislammahim23 <aminulislammahim2013@gmail.com>
2025-08-30 20:06:04 +0600

    add custom error page (404 & 500) update version 8.30.25.2

---
+++ view/errors/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>404 - Page Not Found</title>
+  <link rel="stylesheet" href="/CodeCraft/assets/css/error.css">
+</head>
+<body>
+  <div class="error-container">
+    <h1>404</h1>
+    <p>Oops! The page you’re looking for doesn’t exist.</p>
+    <div class="suggestions">
+      <a href="/CodeCraft/views/home.php" class="btn">🏠 Go Home</a>
+      <a href="/CodeCraft/views/login.php" class="btn">🔑 Login</a>
+      <a href="/CodeCraft/views/help.php" class="btn">❓ Help Center</a>
+    </div>
+  </div>
+</body>
+</html>
