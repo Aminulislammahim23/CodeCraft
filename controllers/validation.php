1aae493d9eebc6a0e9603679c7a689f15477da5f
Upama Prapti <cpcourse9806@gmail.com>
2025-09-01 02:07:53 +0600

    Update validation.php

--- controllers/validation.php
+++ controllers/validation.php
@@ -1,21 +1,29 @@
 <?php
 session_start();
 
-$username = trim($_REQUEST['username'] ?? '');
-$password = trim($_REQUEST['password'] ?? '');
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $username = trim($_POST['username'] ?? '');
+    $password = $_POST['password'] ?? '';
 
-if($username === "" || $password === ""){
-    echo "Please type username/password first!";
-    exit();
-}
-
-if($username === $password){
-    $_SESSION['status'] = true;
-    $_SESSION['username'] = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');
-    header('Location: view/admin.php');
-    exit();
+  
+    if (isset($_SESSION['signup_credentials']) && 
+        $username === $_SESSION['signup_credentials']['username'] && 
+        $password === $_SESSION['signup_credentials']['password']) {
+    
+        $_SESSION['username'] = $username;
+        setcookie('status', 'true', time() + (86400 * 30), "/"); 
+        unset($_SESSION['signup_credentials']); 
+        header("Location: ../view/home.php");
+        
+        exit();
+    } else {
+        
+        header("Location: ../view/login.php?error=invalid_user");
+        exit();
+    }
 } else {
-    header('Location: view/login.php?error=invalid_user');
+    
+    header("Location: ../view/login.php?error=badrequest");
     exit();
 }
 ?>
