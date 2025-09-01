aff5e13bd80f243d7d9499f3e3dd95174c0f5d8e
Upama Prapti <cpcourse9806@gmail.com>
2025-09-01 14:06:02 +0600

    Update signup.php

--- view/signup.php
+++ view/signup.php
@@ -24,6 +24,7 @@ if (isset($_SESSION['signup_errors'])) {
 }
 
 // check
+// check valid
 $old_name = $_SESSION['old_signup_data']['name'] ?? '';
 $old_email = $_SESSION['old_signup_data']['email'] ?? '';
 $old_dob = $_SESSION['old_signup_data']['dob'] ?? '';
