e50e56638750f6f6b74a30d8051f7a1e7b51e445
Aminulislammahim23 <aminulislammahim2013@gmail.com>
2025-09-06 15:45:47 +0600

    update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,8 +1,31 @@
-function showSection(sectionId) {
-      document.querySelectorAll('.section').forEach(sec => sec.style.display = 'none');
-      document.getElementById(sectionId).style.display = 'block';
-    }
+function showSection(id){
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(sec => sec.style.display = 'none');
+
+    const target = document.getElementById(id);
+    if(target) target.style.display = 'block';
+}
+
+// page load default show dashboard
+document.addEventListener('DOMContentLoaded', () => {
+    showSection('dashboard');
+});
 
     function addCourse() {
       alert("New course added (demo)!");
     }
+
+    function addInstructor() {
+      alert("New instructor added (demo)!");
+    }
+
+
+    function showSection(id){
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(sec => sec.style.display = 'none');
+
+    const target = document.getElementById(id);
+    if(target){
+        target.style.display = 'block';
+    }
+}
