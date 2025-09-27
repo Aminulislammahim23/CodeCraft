64e622cd322e45d61bf7bdea4db7e21fe0d66600
Aminulislammahim23 <aminulislammahim2013@gmail.com>
2025-09-28 03:55:06 +0600

    update db pack

--- share db/codecraftdb.sql
+++ share db/codecraftdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2025 at 12:51 AM
+-- Generation Time: Sep 27, 2025 at 11:54 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -73,6 +73,37 @@ INSERT INTO `courses` (`course_id`, `title`, `description`, `instructor_id`, `ca
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `enrollments`
+--
+
+CREATE TABLE `enrollments` (
+  `enrollment_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `enrollment_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `progress` decimal(5,2) DEFAULT 0.00,
+  `status` enum('ongoing','completed','dropped') DEFAULT 'ongoing'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instructors`
+--
+
+CREATE TABLE `instructors` (
+  `instructor_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `bio` text DEFAULT NULL,
+  `expertise` varchar(255) DEFAULT NULL,
+  `total_courses` int(11) DEFAULT 0,
+  `total_students` int(11) DEFAULT 0,
+  `rating` decimal(3,2) DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `messages`
 --
 
@@ -104,6 +135,34 @@ INSERT INTO `messages` (`id`, `sender_id`, `receiver_id`, `message`, `sent_at`, 
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `quizzes`
+--
+
+CREATE TABLE `quizzes` (
+  `quiz_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `title` varchar(150) DEFAULT NULL,
+  `total_marks` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz_attempts`
+--
+
+CREATE TABLE `quiz_attempts` (
+  `attempt_id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `obtained_marks` int(11) DEFAULT NULL,
+  `attempt_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `revenue`
 --
 
@@ -119,6 +178,22 @@ CREATE TABLE `revenue` (
   `payment_status` enum('Pending','Completed','Failed') DEFAULT 'Pending',
   `transaction_id` varchar(100) DEFAULT NULL,
   `payment_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `enroll_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `total_courses_enrolled` int(11) DEFAULT 0,
+  `total_courses_completed` int(11) DEFAULT 0,
+  `learning_streak` int(11) DEFAULT 0,
+  `certificates_earned` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -148,9 +223,12 @@ INSERT INTO `users` (`id`, `name`, `username`, `password`, `email`, `dob`, `role
 (7, 'Rahim Uddin', 'rahim', 'abc123', 'rahim@gmail.com', '1999-08-10', 'instructor', 'default.png'),
 (8, 'Karim Ali', 'karim', 'pass321', 'karim@example.com', '2001-01-22', 'student', 'default.png'),
 (9, 'Nusrat Jahan', 'nusrat', 'mypassword', 'nusrat@example.com', '2002-03-30', 'student', 'default.png'),
-(14, 'Aminul Islam Mahim', 'mahim677', 'mahim@gmail.com', '12345678', '2000-10-22', 'student', 'default.png'),
+(14, 'Aminul Islam Mahim', 'mahim677', '11223344', 'mahim11@gmail.com', '2000-10-22', 'student', 'default.png'),
 (16, 'Aminul Islam Mahim', 'mahim55', '123123', 'mahim222@gmail.com', '2000-10-20', 'instructor', 'assets/uploads/imgP/1758838461_images.webp'),
-(17, 'mim', 'mim', 'mim@gmail.com', '552299', '2000-10-18', 'student', NULL);
+(17, 'mim', 'mim', 'mim@gmail.com', '552299', '2000-10-18', 'student', NULL),
+(18, 'mim11', 'mim11@gmail.com', 'mim@gmail.com', '123123', '2002-12-12', 'student', NULL),
+(20, 'Aminul Islam Mahim', 'mahim121@gmail.com', '123123', '', '2000-10-20', 'student', NULL),
+(24, 'Aminul Islam Mahim', 'mahim113', '11223311', 'mahim1133@gmail.com', '1222-12-11', 'student', NULL);
 
 --
 -- Indexes for dumped tables
@@ -173,12 +251,42 @@ ALTER TABLE `courses`
   ADD KEY `instructor_id` (`instructor_id`);
 
 --
+-- Indexes for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  ADD PRIMARY KEY (`enrollment_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `instructors`
+--
+ALTER TABLE `instructors`
+  ADD PRIMARY KEY (`instructor_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `messages`
 --
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`),
   ADD KEY `sender_id` (`sender_id`),
   ADD KEY `receiver_id` (`receiver_id`);
+
+--
+-- Indexes for table `quizzes`
+--
+ALTER TABLE `quizzes`
+  ADD PRIMARY KEY (`quiz_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `quiz_attempts`
+--
+ALTER TABLE `quiz_attempts`
+  ADD PRIMARY KEY (`attempt_id`),
+  ADD KEY `quiz_id` (`quiz_id`),
+  ADD KEY `student_id` (`student_id`);
 
 --
 -- Indexes for table `revenue`
@@ -188,6 +296,13 @@ ALTER TABLE `revenue`
   ADD UNIQUE KEY `transaction_id` (`transaction_id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -214,22 +329,52 @@ ALTER TABLE `courses`
   MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
+-- AUTO_INCREMENT for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  MODIFY `enrollment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `instructors`
+--
+ALTER TABLE `instructors`
+  MODIFY `instructor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
+-- AUTO_INCREMENT for table `quizzes`
+--
+ALTER TABLE `quizzes`
+  MODIFY `quiz_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quiz_attempts`
+--
+ALTER TABLE `quiz_attempts`
+  MODIFY `attempt_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `revenue`
 --
 ALTER TABLE `revenue`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Constraints for dumped tables
@@ -249,6 +394,19 @@ ALTER TABLE `courses`
   ADD CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`instructor_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
+-- Constraints for table `enrollments`
+--
+ALTER TABLE `enrollments`
+  ADD CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `instructors`
+--
+ALTER TABLE `instructors`
+  ADD CONSTRAINT `instructors_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `messages`
 --
 ALTER TABLE `messages`
@@ -256,11 +414,30 @@ ALTER TABLE `messages`
   ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
+-- Constraints for table `quizzes`
+--
+ALTER TABLE `quizzes`
+  ADD CONSTRAINT `quizzes_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `quiz_attempts`
+--
+ALTER TABLE `quiz_attempts`
+  ADD CONSTRAINT `quiz_attempts_ibfk_1` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`quiz_id`),
+  ADD CONSTRAINT `quiz_attempts_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`);
+
+--
 -- Constraints for table `revenue`
 --
 ALTER TABLE `revenue`
   ADD CONSTRAINT `revenue_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `revenue_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
